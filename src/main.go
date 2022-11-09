217417c4ae99f071b780dc7f7bcd63a4dcb167ae
RyanBreathings <ryan@breathings.co.kr>
2022-11-09 23:12:13 +0900

    환경변수 초기화 완료

--- src/main.go
+++ src/main.go
@@ -6,12 +6,17 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	echoSwagger "github.com/swaggo/echo-swagger"
+	"main/common"
 	_ "main/docs"
 	swaggerDocs "main/docs"
 	"net/http"
 )
 
 func main() {
+	if err := common.InitEnv(); err != nil {
+		fmt.Println("서버 에러 발생")
+		return
+	}
 	e := echo.New()
 	e.Use(middleware.Recover())
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
@@ -21,8 +26,16 @@ func main() {
 	e.GET("/health", func(c echo.Context) error {
 		return c.NoContent(http.StatusOK)
 	})
-	swaggerDocs.SwaggerInfo.Host = "localhost:3000"
+
+	// swagger 초기화
+	if common.Env.IsLocal {
+		swaggerDocs.SwaggerInfo.Host = "localhost:3000"
+		e.GET("/swagger/*", echoSwagger.WrapHandler)
+	} else {
+		swaggerDocs.SwaggerInfo.Host = fmt.Sprintf("%s-%s.breathings.net", common.Env.Env, "ticketing")
+		e.GET("/swagger/*", echoSwagger.WrapHandler)
+	}
 	e.GET("/swagger/*", echoSwagger.WrapHandler)
 	e.HideBanner = true
-	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", "3000")))
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", common.Env.Port)))
 }
