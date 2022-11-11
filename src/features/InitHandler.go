800f1a2a388b9305f412efefa3398f4ff7a31a01
RyanBreathings <ryan@breathings.co.kr>
2022-11-11 21:08:04 +0900

    핸들러 초기화

--- src/features/InitHandler.go
+++ src/features/InitHandler.go
@@ -1,1 +1,11 @@
 package features
+
+import (
+	"github.com/labstack/echo/v4"
+	googleOAuthHandler "main/features/oauth/google/handler"
+)
+
+func InitHandler(e *echo.Echo) {
+	gApiV01 := e.Group("/v0.1")
+	googleOAuthHandler.RegisterGoogleOAuthHandler(gApiV01)
+}
--- src/features/oauth/google/handler/index.go
+++ src/features/oauth/google/handler/index.go
@@ -2,8 +2,9 @@ package handler
 
 import "github.com/labstack/echo/v4"
 
-func RegisterAddressBookHandler(e *echo.Group) {
+func RegisterGoogleOAuthHandler(e *echo.Group) {
 	handler := NewGoogleOAuthHandler()
-	api := e.Group("/v0.1")
-	api.POST("/v0.1", handler.SignInGoogleOAuthHandler.SignInGoogle)
+	api := e.Group("/auth/google")
+	api.POST("/login", handler.SignInGoogleOAuthHandler.SignInGoogle)
+	api.POST("/callback", handler.CallbackGoogleOAuthHandler.GoogleSignInCallback)
 }
