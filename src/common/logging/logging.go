f0c61a2369a001db42b86dfd05904e73c7beafac
RyanBreathings <ryan@breathings.co.kr>
2022-11-13 19:03:10 +0900

    로깅 틀 잡았음. 어떤식으로 데이터를 가져올지 정리 필요

--- src/common/logging/logging.go
+++ src/common/logging/logging.go
@@ -1,28 +1,59 @@
 package logging
 
-type AccessLog struct {
-	Project   string `json:"project"`
-	Type      string `json:"type"`
-	UserID    string `json:"userID"`
-	Url       string `json:"url"`
-	Method    string `json:"method"`
-	Latency   int    `json:"latency"`
-	HttpCode  int    `json:"httpCode"`
-	RequestID string `json:"requestID"`
+import (
+	"github.com/rs/zerolog/log"
+	"main/common/env"
+	"time"
+)
+
+type Logging interface {
+	MakeLog() error
+	MakeErrorLog() error
 }
 
-type ErrorLog struct {
-	Project      string                 `json:"project"`
-	Type         string                 `json:"type"`
-	UserID       string                 `json:"userID"`
-	Url          string                 `json:"url"`
-	Method       string                 `json:"method"`
-	Latency      int                    `json:"latency"`
-	HttpCode     int                    `json:"httpCode"`
-	RequestID    string                 `json:"requestID"`
+type Log struct {
+	Project   string    `json:"project"`
+	Type      string    `json:"type"`
+	UserID    string    `json:"userID"`
+	Url       string    `json:"url"`
+	Method    string    `json:"method"`
+	Latency   int64     `json:"latency"`
+	HttpCode  int       `json:"httpCode"`
+	RequestID string    `json:"requestID"`
+	ErrorInfo ErrorInfo `json:"errorInfo,omitempty"`
+}
+
+type ErrorInfo struct {
 	Stack        string                 `json:"stack"`
 	ErrorType    string                 `json:"errorType"`
 	Err          string                 `json:"err"`
 	From         string                 `json:"from"`
 	RequestParam map[string]interface{} `json:"requestParam"`
+}
+
+func (l *Log) MakeLog(userID string, url string, method string, startTime time.Time, httpCode int, requestID string) error {
+	l.Project = env.Env.Project
+	l.Type = "access"
+	l.UserID = userID
+	l.Url = url
+	l.Method = method
+	l.Latency = time.Since(startTime).Milliseconds()
+	l.HttpCode = httpCode
+	l.RequestID = requestID
+	return nil
+}
+func (l *Log) MakeErrorLog() error {
+	l.Type = "error"
+
+	return nil
+}
+
+func (l *Log) InfoLog() error {
+	log.Info().Str("project", l.Project)
+	//	log.Info().Str("URI", url).Int("state", 200).Int64("latency", time.Since(startTime).Milliseconds()).Str("METHOD", "POST").Msg("hi")
+	return nil
+}
+func (l *Log) ErrorLog() error {
+	log.Error().Str("project", l.Project)
+	return nil
 }
--- src/middleware/logging.go
+++ src/middleware/logging.go
@@ -3,7 +3,7 @@ package middleware
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/gommon/random"
-	"github.com/rs/zerolog/log"
+	"main/common/logging"
 	"time"
 )
 
@@ -12,15 +12,20 @@ func RestLogger(next echo.HandlerFunc) echo.HandlerFunc {
 		startTime := time.Now()
 		req := c.Request()
 		url := req.URL.Path
-		c.Set("rID", random.String(32))
+		rID := random.String(32)
+		c.Set("rID", rID)
 		err := next(c)
 
+		//로그 데이터 생성한다.
+		logData := logging.Log{}
+		logData.MakeLog("", url, "", startTime, 200, rID)
+		//여기 나와야 로그를 찍을 수 있으니 로그 데이터를 만든다.
 		if err != nil {
-			//에러 로그 생성
-			log.Error().Err(err).Msg("hi")
+			logData.MakeErrorLog()
+			logData.ErrorLog()
 		} else {
-			//일반 로그 생성
-			log.Info().Str("URI", url).Int("state", 200).Int64("latency", time.Since(startTime).Milliseconds()).Str("METHOD", "POST").Msg("hi")
+			//로그 찍는다. (local 일때만 찍는걸로 데이터 쌓이면 돈 많이 나가니..)
+			logData.InfoLog()
 		}
 
 		return nil
