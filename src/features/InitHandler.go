31241f4379d3e082390f423aa0122ae8eb9d3c0f
RyanBreathings <ryan@breathings.co.kr>
2022-11-14 19:19:55 +0900

    test

--- src/features/InitHandler.go
+++ src/features/InitHandler.go
@@ -12,6 +12,9 @@ func InitHandler(e *echo.Echo) error {
 	e.GET("/health", func(c echo.Context) error {
 		return c.NoContent(http.StatusOK)
 	})
+	e.GET("/health/ok", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, "ok")
+	})
 	gApiV01 := e.Group("/v0.1")
 	gApiV01.Use(middleware.RestLogger)
 	googleOAuthHandler.RegisterGoogleOAuthHandler(gApiV01)
