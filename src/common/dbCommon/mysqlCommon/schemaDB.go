589abbd244cbfeb8fed170d0dfd9c2b2c8aa614c
RyanBreathings <ryan@breathings.co.kr>
2022-11-27 05:21:00 +0900

    feat : 상품 db 스키마 추가 상품 디비 스키마 정의

--- src/common/dbCommon/mysqlCommon/schemaDB.go
+++ src/common/dbCommon/mysqlCommon/schemaDB.go
@@ -16,3 +16,17 @@ type UserAuth struct {
 	Created    string `json:"created"`    //생성 날짜
 	IsDeleted  bool   `json:"isDeleted"`  //활동 여부
 }
+
+type Product struct {
+	ID          string `json:"id"`          //pk
+	Created     string `json:"created"`     //생성 날짜
+	LastUpdated string `json:"lastUpdated"` //마지막 수정 날짜
+	IsDeleted   bool   `json:"isDeleted"`   //삭제 유무
+	Name        string `json:"name"`        // 상품이름
+	Description string `json:"description"` //상품 설명
+	Category    string `json:"category"`    //상품 카테고리
+	PerAmount   int64  `json:"perAmount"`   //상품 티켓 당 금액
+	ImgUrl      string `json:"imgUrl"`      //이미지
+	TotalCount  int64  `json:"totalCount"`  //총 수량
+	RestCount   int64  `json:"restCount"`   //남은 수량
+}
